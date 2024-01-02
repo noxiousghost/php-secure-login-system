0f8d9adb1fe347cb9cf96c13dc2009ac8fbf23e9
NarayanSapkota <59175636+noxiousghost@users.noreply.github.com>
2024-01-02 22:06:17 +0545

    code cleanup

--- send_email.php
+++ send_email.php
@@ -6,35 +6,26 @@ require './PHPMailer/src/Exception.php';
 require './PHPMailer/src/PHPMailer.php';
 require './PHPMailer/src/SMTP.php';
 
-
 function sendMail($recipient, $subject, $message) {
-    // Create a PHPMailer object
+    // PHPMailer object
     $mail = new PHPMailer(true);
-
     try {
-        // Set mailer to use SMTP
         $mail->isSMTP();
-
-        // Enable SMTP debugging if needed
-        // $mail->SMTPDebug = SMTP::DEBUG_SERVER;
-
-        // Set the SMTP server details
+        // SMTP server details
         $mail->Host = "smtp.gmail.com";
         $mail->Port = 587; // Use 465 for SSL
         $mail->SMTPAuth = true;
         $mail->SMTPSecure = 'tls'; // Use 'ssl' for SSL
-
-        // Set the SMTP username and password
+        // SMTP username and password
+        // these are my own credentials you can add your own if you like
         $mail->Username = "sapkota.king@gmail.com";
         $mail->Password = "dxkzwuyxdzeixzqi";
-
-        // Set the 'from' address, recipient, subject, and message body
+        // setting the 'from' address, recipient, subject, and message body
         $mail->setFrom("sapkota.king@gmail.com", "Narayan Sapkota");
         $mail->addAddress($recipient);
         $mail->Subject = $subject;
         $mail->Body = $message;
-
-        // Send the email
+        // sneding the email
         $mail->send();
         return true;
     } catch (Exception $e) {
